85bb9f525b963dea52998ca4913d4bc692cdf081
Kyle R Schulman <ks82852@srcs.loc>
2020-02-24 10:21:29 -0800

    Initial JS kyle

---
+++ app.js
@@ -0,0 +1,13 @@
+var pres = document.createElement("img");
+
+let potus = document.getElementById("my_button");
+
+potus.addEventListener("click",()=> {
+
+}
+
+function showPres(){
+    var id = document.getElementById();
+    document.body.appendChild(potus);
+    var pic = []
+}
